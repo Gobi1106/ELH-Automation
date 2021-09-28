91c47489c2d64eeb2cb19f434e17f4459e10f6aa
unknown <sriniseenu02@gmail.com>
2021-09-28 13:23:16 +0530

    cypress updated script

--- CypressAutomationTesting/cypress/integration/Main.js
+++ CypressAutomationTesting/cypress/integration/Main.js
@@ -11,59 +11,12 @@ import "cypress-xpath"
 import iframe from '../support/POM/iframe';
 describe('Test Suite', ()=>{
    
-   // var result,firstname,lastname,address,email,phno,gender,hobbies,selectCountry,year,substryear,month,password,confirmpwd;
    beforeEach(function(){
       cy.viewport(1920,1080)
-      // const test= require('../fixtures/arrayData.json')
-      // for(let i=0;i<test.Regno.length;i++){
-      //    cy.log(test.Regno.length)
-      //    cy.log(i)
-      // firstname= test.Regno[i].Firstname
-      // cy.log(firstname)
-      // lastname=test.Regno[i].Lastname
-      // address=test.Regno[i].Address
-      // email=test.Regno[i].Email
-      // phno=test.Regno[i].Phno
-      // gender=test.Regno[i].Gender
-      // hobbies=test.Regno[i].Hobbies
-      // month=test.Regno[i].Month
-      // year=test.Regno[i].Year
-      // substryear=test.Regno[i].Substryear
-      // selectCountry=test.Regno[i].SelectCountry
-      // password=test.Regno[i].Password
-      // confirmpwd=test.Regno[i].ConfirmPassword
-      // }
-
-      // cy.fixture('data').then((res)=>{
-      //    this.result=res;
-      //    firstname=this.result.Firstname;
-      //    lastname=this.result.Lastname;
-      //    address=this.result.Address;
-      //    email=this.result.Email;   
-      //    phno=this.result.Phno;
-      //    gender=this.result.Gender;
-      //    hobbies=this.result.Hobbies;
-      //    month=this.result.Month;
-      //    year=this.result.Year;
-      //    substryear=this.result.Substryear;
-      //    selectCountry=this.result.SelectCountry;
-      //    password=this.result.Password;
-      //    confirmpwd=this.result.ConfirmPassword;
-
-      // })
    })
    afterEach(function(){
       cy.log("Check by yourself, if it is completed or not")
    })
-
-// beforeEach(function(){
-//      cy.fixture('data').then(function(res){
-//         this.res=res;
-//         firstname=this.res.firstname;
-//         lastname=this.res.lastname;
-//         address=this.res.Address;
-//      })
-// })
 
  it('Register page',function(){
    //cy.viewport(1920,1080)
@@ -76,13 +29,9 @@ describe('Test Suite', ()=>{
    firstname= test.Regno[i].Firstname
    cy.log(firstname)
    lastname=test.Regno[i].Lastname
-   //address=test.Regno[i].Address
-   //email=test.Regno[i].Email
-   //phno=test.Regno[i].Phno
    gender=test.Regno[i].Gender
    hobbies1=JSON.stringify(test.Regno[i].Hobbies[0].cricket)
    hobbies2=JSON.stringify(test.Regno[i].Hobbies[0].movies)
-   //hobbies3=JSON.stringify(test.Regno[i].Hobbies[0].hockey)
    cy.log(hobbies1)
    cy.log(hobbies2)
 
@@ -131,50 +80,6 @@ it.only('jsondata',function(){
 it('date',function(){
    cy.visit("http://demo.automationtesting.in/Datepicker.html")
 
-   // cy.get("input#datepicker1").
-   // click().type(getdate(),{ force: true })
-   // function getdate(){
-
-   // var dob;
-   // //set a range of years
-   // var min = 2018;
-   // var max = 2021;
-   
-   // // Math.ceil prevents the value from being 0;
-   // var month = Math.ceil(Math.random() * 12);
-   // var day = Math.ceil(Math.random() * 28);
-   // var year = Math.floor(Math.random() * (max - min) + min);
-   
-   // //this ensures that the format will stay mm/dd/yyyy;
-   // if(month < 10) {
-   //    month = "0" + month;
-   // }
-   // if(day < 10) {
-   //    day = "0" + day;
-   // }
-   // //concatenates random dob in mm/dd/yyyy format;
-   // dob = month + "/" + day + "/" + year;
-   
-   // return dob;
-   // }
-
-
-
-
-
-  
-   // const date = randomDate(new Date(2021, 0, 1), new Date());
-   // var str=date.substr(0,15)
-   // var res=str.substring(4)
-   // cy.log(res)
-   // var hob1=res.replace(/\s+/g, "/");
-
-   // function randomDate(start, end) {
-   //    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toString();
-   //    }
-   //    cy.get("input#datepicker1").
-   //    click().type(hob1)
-   //    cy.log(hob1);
         const moment= require('moment')
          //const moment = require('..//fixtures/date.json');
            var date = new Date();
@@ -188,14 +93,6 @@ it('date',function(){
            cy.log(dateToSelect)
            cy.get("input#datepicker1").
            click().type(dateToSelect,{force: true})
-           //test.logOutPut("split ==" + split)
-         //   if (split.includes("")) {
-         //   dateToSelect = dateToSelect.toString().replace(split, split.charAt(1) + ",");
-         //   cy.log(dateToSelect)
-         //   }
-   // register.randomDate()
-   // cy.reload()
-   // register.randomDate()
 })
 
  })
---
+++ CypressAutomationTesting/cypress/support/POM/readExcel1.js
@@ -0,0 +1,10 @@
+const XLSX = require('xlsx');
+const fs=require('fs')
+
+const read=XLSX.readFile("./cypress/fixtures/Demo.xlsx")
+console.log(read.SheetNames);
+//const sheet=read.Sheets["sheet"]
+const readExcel=XLSX.utils.sheet_to_row_object_array(read.Sheets["Sheet1"])
+console.log(readExcel)
+fs.writeFileSync("./cypress/fixtures/newJson.js",JSON.stringify(readExcel,null,2),'utf-8')
+
